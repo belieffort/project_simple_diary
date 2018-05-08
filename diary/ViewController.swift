4aa2fde8ae6e5b47a366d2c93c10e1fb5887b88a
한병두 <ByungdooHan@hanbyeongduui-macbook-pro.local>
2018-05-08 14:32:58 +0900

    Initial Commit

---
+++ diary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  diary
+//
+//  Created by 한병두 on 2018. 5. 8..
+//  Copyright © 2018년 Byungdoo Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
